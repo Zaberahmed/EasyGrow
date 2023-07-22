f249ba1f4bfdad7bedd4ad62268388e20410f6cf
Sk. Zaber Ahmed <zaber0305@gmail.com>
2023-07-22 18:24:46 +0600

    stupid person push

--- server/src/controllers/landOwner.contorller.ts
+++ server/src/controllers/landOwner.contorller.ts
@@ -5,102 +5,102 @@ import { getAllOffersByLandId, changeOfferStatus } from '../models/offer.model';
 
 // Land controllers
 const addLand = async (req: Request, res: Response) => {
-  try {
-    const { name, size, ownerId, location, description, price } = req.body;
+	try {
+		const { name, size, ownerId, location, description, price } = req.body;
 
-    const newLand = {
-      name,
-      size,
-      ownerId,
-      location,
-      description,
-      price,
-    };
+		const newLand = {
+			name,
+			size,
+			ownerId,
+			location,
+			description,
+			price,
+		};
 
-    const createdLand = await addALand(newLand);
+		const createdLand = await addALand(newLand);
 
-    if (createdLand && createdLand._id) {
-      const landOwner = await addALandByLandId(ownerId, createdLand._id);
+		if (createdLand && createdLand._id) {
+			const landOwner = await addALandByLandId(ownerId, createdLand._id);
 
-      if (landOwner) {
-        res.status(200).send(createdLand);
-      }
-    }
-  } catch (error) {
-    res.status(500);
-    console.log(error);
-  }
+			if (landOwner) {
+				res.status(200).send(createdLand);
+			}
+		}
+	} catch (error) {
+		res.status(500);
+		console.log(error);
+	}
 };
 
 const removeLand = async (req: Request, res: Response) => {
-  try {
-    const { userId, landId } = req.body;
+	try {
+		const { userId, landId } = req.body;
 
-    await removeALandByLandId(userId, landId);
-    await removeALand(landId);
+		await removeALandByLandId(userId, landId);
+		await removeALand(landId);
 
-    res.status(200).send('Land removed successfully');
-  } catch (error) {
-    res.status(500);
-    console.log(error);
-  }
+		res.status(200).send('Land removed successfully');
+	} catch (error) {
+		res.status(500);
+		console.log(error);
+	}
 };
 
 const landSearchByOwner = async (req: Request, res: Response) => {
-  try {
-    const { ownerId } = req.body;
+	try {
+		const { ownerId } = req.body;
 
-    const lands = await searchLandByOwner(ownerId);
+		const lands = await searchLandByOwner(ownerId);
 
-    res.status(200).send(lands);
-  } catch (error) {
-    res.status(500);
-    console.log(error);
-  }
+		res.status(200).send(lands);
+	} catch (error) {
+		res.status(500);
+		console.log(error);
+	}
 };
 
 //Offer controllers
 const allOffersForALand = async (req: Request, res: Response) => {
-  try {
-    const { landId } = req.body;
+	try {
+		const { landId } = req.body;
 
-    const allOffers = await getAllOffersByLandId(landId);
+		const allOffers = await getAllOffersByLandId(landId);
 
-    res.status(200).send(allOffers);
-  } catch (error) {
-    res.status(500);
-    console.log(error);
-  }
+		res.status(200).send(allOffers);
+	} catch (error) {
+		res.status(500);
+		console.log(error);
+	}
 };
 
 const acceptOffer = async (req: Request, res: Response) => {
-  try {
-    const { offerId, status } = req.body;
+	try {
+		const { offerId, status } = req.body;
 
-    const newStatus = await changeOfferStatus(offerId, status);
+		const newStatus = await changeOfferStatus(offerId, status);
 
-    if (newStatus) {
-      res.status(200).send('Offer accepted');
-    }
-  } catch (error) {
-    res.status(500);
-    console.log(error);
-  }
+		if (newStatus) {
+			res.status(200).send('Offer accepted');
+		}
+	} catch (error) {
+		res.status(500);
+		console.log(error);
+	}
 };
 
 const rejectOffer = async (req: Request, res: Response) => {
-  try {
-    const { offerId, status } = req.body;
+	try {
+		const { offerId, status } = req.body;
 
-    const newStatus = await changeOfferStatus(offerId, status);
+		const newStatus = await changeOfferStatus(offerId, status);
 
-    if (newStatus) {
-      res.status(200).send('Offer rejected');
-    }
-  } catch (error) {
-    res.status(500);
-    console.log(error);
-  }
+		if (newStatus) {
+			res.status(200).send('Offer rejected');
+		}
+	} catch (error) {
+		res.status(500);
+		console.log(error);
+	}
 };
 
 export { addLand, removeLand, landSearchByOwner, allOffersForALand, acceptOffer, rejectOffer };
--- server/src/middlewares/authorizer.ts
+++ server/src/middlewares/authorizer.ts
@@ -3,16 +3,16 @@ import { getSession, SessionData } from './../middlewares/sessionManagement';
 import { findUserByEmail } from './../models/user.model';
 
 const authorizer = async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.accessToken;
-  const session: SessionData | undefined = getSession(token);
+	const token = req.cookies.accessToken;
+	const session: SessionData | undefined = getSession(token);
 
-  if (session) {
-    const user = await findUserByEmail(session.userEmail);
+	if (session) {
+		const user = await findUserByEmail(session.userEmail);
 
-    if (user?.role === 'farmer' || user?.role === 'landOwner') {
-      next();
-    }
-  } else res.status(404).send('You are not authorized !!!');
+		if (user?.role === 'farmer' || user?.role === 'landOwner') {
+			next();
+		}
+	} else res.status(404).send('You are not authorized !!!');
 };
 
 export { authorizer };
