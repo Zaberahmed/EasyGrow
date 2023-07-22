d3302b4d06cf1fcc1f000f5b59f73b27218ffd3b
Sk. Zaber Ahmed <zaber0305@gmail.com>
2023-07-22 14:48:41 +0600

    modify: .env file

--- server/src/models/database.ts
+++ server/src/models/database.ts
@@ -1,7 +1,8 @@
 import mongoose, { Schema, Types, model } from 'mongoose';
+import path from 'path';
 
-require('dotenv').config({ path: __dirname + '../.env' });
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
-mongoose.connect(`mongodb://127.0.0.1:27017/EasyGrow_Test`);
+mongoose.connect(`mongodb://127.0.0.1:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`);
 
 export { Schema, Types, model };
