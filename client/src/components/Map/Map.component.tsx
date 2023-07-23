c30221515bf631346594448191e79312a5481652
Sk. Zaber Ahmed <zaber0305@gmail.com>
2023-07-23 10:40:43 +0600

    modify: map logic

--- client/src/components/Map/Map.component.tsx
+++ client/src/components/Map/Map.component.tsx
@@ -1,6 +1,6 @@
 import './Map.style.css';
-import React, { useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl';
+import { useEffect, useRef } from 'react';
+import mapboxgl, { Map } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
@@ -8,82 +8,81 @@ import MapboxGeocoder from 'mapbox-gl-geocoder';
 import 'mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import * as turf from '@turf/turf';
 
-const MAPBOX_ACCESS_TOKEN =
-  'pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g';
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g';
 
-const MapComponent: React.FC = () => {
-  const mapContainerRef = useRef<HTMLDivElement>(null);
+const MapComponent = () => {
+	const mapContainerRef = useRef<HTMLDivElement>(null);
+	const mapRef = useRef<Map | null>(null);
 
-  useEffect(() => {
-    mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current!,
-      style: 'mapbox://styles/mapbox/satellite-streets-v12',
-      center: [-79.4512, 43.6568],
-      zoom: 13,
-    });
+	useEffect(() => {
+		if (mapContainerRef.current) {
+			mapRef.current = new mapboxgl.Map({
+				container: mapContainerRef.current,
+				style: 'mapbox://styles/mapbox/satellite-streets-v12?optimize=true',
+				center: [-0.1084, 51.5549],
+				zoom: 14,
+			});
 
-    map.addControl(
-      new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl,
-      })
-    );
+			mapRef.current.addControl(
+				new MapboxGeocoder({
+					accessToken: mapboxgl.accessToken,
+					mapboxgl: mapboxgl,
+				})
+			);
 
-    const draw = new MapboxDraw({
-      displayControlsDefault: false,
-      controls: {
-        polygon: true,
-        trash: true,
-      },
-      defaultMode: 'draw_polygon',
-    });
-    map.addControl(draw);
+			const draw = new MapboxDraw({
+				displayControlsDefault: false,
+				controls: {
+					polygon: true,
+					trash: true,
+				},
+				defaultMode: 'draw_polygon',
+			});
+			mapRef.current.addControl(draw);
 
-    map.on('draw.create', updateArea);
-    map.on('draw.delete', updateArea);
-    map.on('draw.update', updateArea);
+			mapRef.current.on('draw.create', updateArea);
+			mapRef.current.on('draw.delete', updateArea);
+			mapRef.current.on('draw.update', updateArea);
 
-    function updateArea(e: any) {
-      const data = draw.getAll();
-      console.log(data);
+			function updateArea(e: any) {
+				const data = draw.getAll();
+				console.log(data);
 
-      const center = turf.center(data);
+				const center = turf.center(data);
 
-      new mapboxgl.Marker({ color: 'red', anchor: 'center' })
-        .setLngLat([center.geometry.coordinates[0], center.geometry.coordinates[1]])
-        .setPopup(new mapboxgl.Popup({ offset: 25 }).setText('Hello'))
-        .addTo(map);
+				if (mapRef.current) {
+					new mapboxgl.Marker({ color: 'red', anchor: 'center' })
+						.setLngLat([center.geometry.coordinates[0], center.geometry.coordinates[1]])
+						.setPopup(new mapboxgl.Popup({ offset: 25 }).setText('Hello'))
+						.addTo(mapRef.current);
+				}
 
-      const answer = document.getElementById('calculated-area');
-      if (data.features.length > 0) {
+				const answer = document.getElementById('calculated-area');
+				if (data.features.length > 0) {
+					const area = turf.area(data);
+					const rounded_area = Math.round(area * 100) / 100;
+					if (answer) answer.innerHTML = `<p><strong>${rounded_area}</strong></p><p>square meters</p>`;
+				} else {
+					if (answer) answer.innerHTML = '';
+					if (e.type !== 'draw.delete') alert('Click on the map to draw a polygon.');
+				}
+			}
+		}
+		return () => mapRef.current?.remove();
+	}, []);
 
-        const area = turf.area(data);
-        const rounded_area = Math.round(area * 100) / 100;
-        if (answer)
-          answer.innerHTML = `<p><strong>${rounded_area}</strong></p><p>square meters</p>`;
-      } else {
-        if (answer) answer.innerHTML = '';
-        if (e.type !== 'draw.delete') alert('Click the map to draw a polygon.');
-      }
-    }
-
-    return () => map.remove();
-  }, []);
-
-  return (
-    <div>
-      <div
-        ref={mapContainerRef}
-        style={{ position: 'absolute', top: 0, bottom: 0, width: '100%', height: '80%' }}
-      />
-      <div className='calculation-box'>
-        {/* <p>Click the map to draw a polygon.</p> */}
-        <div id='calculated-area' />
-      </div>
-      <button className='map-btn'>Add land</button>
-    </div>
-  );
+	return (
+		<div>
+			<div
+				ref={mapContainerRef}
+				style={{ position: 'absolute', top: 0, bottom: 0, width: '100%', height: '80%' }}
+			/>
+			<div className="calculation-box">
+				<div id="calculated-area" />
+			</div>
+			<button className="map-btn">Add land</button>
+		</div>
+	);
 };
 
 export default MapComponent;
--- client/src/components/PhotoWithOverlay/PhotoWithOverlay.tsx
+++ client/src/components/PhotoWithOverlay/PhotoWithOverlay.tsx
@@ -1,39 +1,44 @@
-import Greetings from "../Greetings/Greetings";
-import slider from './../../../assets/land-plot.jpg'
-import { Box, HStack, Heading } from "@chakra-ui/react";
-
+import Greetings from '../Greetings/Greetings';
+import slider from './../../assets/land-plot.jpg';
+import { Box, HStack, Heading } from '@chakra-ui/react';
 
 const PhotoWithOverlay = () => {
-    const styles = {
-        backgroundImage: `url(${slider})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        borderRadius: '25px',
+	const styles = {
+		backgroundImage: `url(${slider})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+		borderRadius: '25px',
 
-        height: '150px'
-    };
-    const overStyles = {
-        fontSize: '1.1rem',
-        color: 'white',
+		height: '150px',
+	};
+	const overStyles = {
+		fontSize: '1.1rem',
+		color: 'white',
 
-        borderRadius: '25px',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        height: '150px'
-
-    };
-    return (
-        <Box style={styles} shadow={'xl'}>
-            <Box style={overStyles} padding={4}>
-                <HStack>
-                    <Greetings />
-                </HStack>
-                <br />
-                <Heading as='h3' size='lg'>
-                    Find the land & <br />Connected with Farmers.
-                </Heading>
-            </Box>
-        </Box>
-    );
+		borderRadius: '25px',
+		backgroundColor: 'rgba(0, 0, 0, 0.5)',
+		height: '150px',
+	};
+	return (
+		<Box
+			style={styles}
+			shadow={'xl'}>
+			<Box
+				style={overStyles}
+				padding={4}>
+				<HStack>
+					<Greetings />
+				</HStack>
+				<br />
+				<Heading
+					as="h3"
+					size="lg">
+					Find the land & <br />
+					Connected with Farmers.
+				</Heading>
+			</Box>
+		</Box>
+	);
 };
 
 export default PhotoWithOverlay;
